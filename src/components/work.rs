9b55afb653555ce9b3cf43e93398474f3e89ae5a
denizhoroz <denizhoroz.ofcl@gmail.com>
2026-08-18 03:03:05 +0300

    added git buttons to projects

--- src/components/work.rs
+++ src/components/work.rs
@@ -1,6 +1,8 @@
 use dioxus::prelude::*;
 use crate::data::projects;
 
+const GITHUB_ICON: Asset = asset!("/assets/icons/github.svg");
+
 #[component]
 pub fn WorkPage(slug: String) -> Element {
     let project = projects().into_iter().find(|p| p.slug == slug);
@@ -10,7 +12,6 @@ pub fn WorkPage(slug: String) -> Element {
             div {
                 id: "work-page",
                 class: "page-block",
-                style: "gap: 20px;",
                 
                 h1 { 
                     class: "block-subtitle",
@@ -32,6 +33,13 @@ pub fn WorkPage(slug: String) -> Element {
                     for t in p.tech.iter() {
                         span { class: "tech-tag", key: "{t}", "{t}" }
                     }
+                }
+
+                a {
+                    class: "block-desc button",
+                    href: p.gitlink,
+                    img { style: "width: 40px", src: GITHUB_ICON, alt: "GitHub", class: "social-icon"} 
+                    "Project GitHub Link"
                 }
             }
         },
--- src/data/projects.rs
+++ src/data/projects.rs
@@ -5,6 +5,7 @@ pub const IMG_PR2: Asset = asset!("/assets/img/project-img2.png");
 pub const IMG_PR3: Asset = asset!("/assets/img/project-img3.png");
 pub const IMG_PR4: Asset = asset!("/assets/img/project-img4.jpg");
 pub const IMG_PR5: Asset = asset!("/assets/img/project-img5.png");
+pub const IMG_PR6: Asset = asset!("/assets/img/project-img6.png");
 
 // Project Box Props
 #[derive(Props, Clone, PartialEq)]
@@ -14,7 +15,7 @@ pub struct ProjectBoxProps {
     pub tech: Vec<String>,
     pub description: String,
     pub image: Asset,
-    pub link: Option<String>,
+    pub gitlink: String,
 }
 
 pub fn projects() -> Vec<ProjectBoxProps> {
@@ -24,7 +25,7 @@ pub fn projects() -> Vec<ProjectBoxProps> {
             title: "Laser Turret Project".to_string(),
             description: "An affordable, fully autonomous laser turret that detects, tracks, and fires at targets using AI-based image processing. Built as a Mechatronics Engineering graduation project at Istanbul Ticaret University.".to_string(),
             image: IMG_PR1,
-            link: None,
+            gitlink: "https://github.com/denizhoroz/laser-turret-project".to_string(),
             tech: vec!["Python".to_string(), "FastAPI".to_string(), "Ultralytics YOLO".to_string(), "Javascript".to_string(), "Arduino (C/C++)".to_string()],
         },
         ProjectBoxProps {
@@ -32,7 +33,7 @@ pub fn projects() -> Vec<ProjectBoxProps> {
             title: "Interloper - Language Learning Platform".to_string(),
             description: "A language learning platform which you can practice in with simulated environments.".to_string(),
             image: IMG_PR2,
-            link: None,
+            gitlink: "https://github.com/denizhoroz/interloper".to_string(),
             tech: vec!["Python".to_string(), "Flask".to_string(), "Langchain".to_string(), "Javascript".to_string()],
         },
         ProjectBoxProps {
@@ -40,7 +41,7 @@ pub fn projects() -> Vec<ProjectBoxProps> {
             title: "Journal Lite".to_string(),
             description: "A lightweight Python journaling application designed to be fast, simple, and focused on daily entries with useful productivity features.".to_string(),
             image: IMG_PR3,
-            link: None,
+            gitlink: "https://github.com/denizhoroz/journal-lite".to_string(),
             tech: vec!["Python".to_string(), "Qt".to_string(), "SQLite".to_string()],
         },
         ProjectBoxProps {
@@ -48,7 +49,7 @@ pub fn projects() -> Vec<ProjectBoxProps> {
             title: "E-Commerce Session Value Prediction Project".to_string(),
             description: "Built an e-commerce prediction pipeline to generate features and parse data using Pandas, and applies random forest classifier to predict session value in BTK Datathon 2025.".to_string(),
             image: IMG_PR4,
-            link: None,
+            gitlink: "https://github.com/denizhoroz/btk-datathon-2025".to_string(),
             tech: vec!["Python".to_string(), "Pandas".to_string(), "Matplotlib".to_string(), "XGBoost".to_string(), "LightGBM".to_string(), "Scikit-learn".to_string()],
         },
         ProjectBoxProps {
@@ -56,8 +57,16 @@ pub fn projects() -> Vec<ProjectBoxProps> {
             title: "Address Resolution Project".to_string(),
             description: "Built an address resolution pipeline that processes noisy and unstructured address texts, then uses hybrid NLP pipelines to find an address value in TEKNOFEST 2025 Address Resolution Hackathon.".to_string(),
             image: IMG_PR5,
-            link: None,
+            gitlink: "https://github.com/denizhoroz/address-resolution".to_string(),
             tech: vec!["Python".to_string(), "Regex".to_string(), "NLP".to_string(), "BERT".to_string(), "TF-IDF".to_string(), "FAISS".to_string()],
+        },
+        ProjectBoxProps {
+            slug: "fusionfm".to_string(),
+            title: "FusionFM".to_string(),
+            description: "A Windows 98 themed website which includes a radio for listening to music and a messaging app to chat with other people on the website.".to_string(),
+            image: IMG_PR6,
+            gitlink: "https://github.com/denizhoroz/fusionfm".to_string(),
+            tech: vec!["Javascript".to_string(), "Node.js".to_string()],
         },
     ]
 }
